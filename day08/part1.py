0fc1d4b32544aebb68e9d4777c0ddcf338a0cc9b
Daniele Venier <danielevenier1998@gmail.com>
2019-12-08 15:23:55 +0100

    Reformat day 8

--- day08/part1.py
+++ day08/part1.py
@@ -4,10 +4,7 @@ h = 6
 s = w * h
 
 layers = [raw[s*i:s*(i+1)] for i in range(int(len(raw)/s))]
-zero_counts = list(map(
-    lambda l: l.count('0'),
-    layers
-))
+zero_counts = list(map(lambda l: l.count('0'), layers))
 min_idx = min([(v, i) for (i, v) in enumerate(zero_counts)])[1]
 
 print(layers[min_idx].count('1')*layers[min_idx].count('2'))
