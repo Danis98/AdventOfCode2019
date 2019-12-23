4d822a1ef7009d281e7c52c1d4cd74add0c0e3ee
Daniele Venier <danielevenier1998@gmail.com>
2019-12-23 07:40:57 +0100

    Day 23

---
+++ day23/part2.py
@@ -0,0 +1,169 @@
+import threading
+tape = dict([(i, int(x)) for (i, x) in enumerate(open("day23.input", "r").read().replace("\n", "").strip().split(","))])
+
+VM_NUM = 50
+
+class IntcodeVM:
+    instr_len = {
+        1: 3,
+        2: 3,
+        3: 1,
+        4: 1,
+        5: 2,
+        6: 2,
+        7: 3,
+        8: 3,
+        9: 1,
+        99: 0
+    }
+
+    memo = {
+        1: "add",
+        2: "mul",
+        3: "in",
+        4: "out",
+        5: "jnz",
+        6: "jz",
+        7: "lt",
+        8: "eq",
+        9: "base",
+        99: "halt"
+    }
+
+    def __init__(self, tape):
+        self.tape = tape.copy()
+        self.pc = 0
+        self.rel_base = 0
+        self.breakpoints = set()
+        self.exec = True
+
+    def get_addr(self, addr, p_mode):
+        if addr not in self.tape:
+            self.tape[addr] = 0
+        if p_mode == 0:
+            if self.tape[addr] not in self.tape:
+                self.tape[self.tape[addr]] = 0
+            return self.tape[addr]
+        elif p_mode == 1:
+            return addr
+        elif p_mode == 2:
+            if self.tape[addr] + self.rel_base not in self.tape:
+                self.tape[self.tape[addr] + self.rel_base] = 0
+            return self.tape[addr] + self.rel_base
+        else:
+            print("AAAAAAA p_mode=", p_mode)
+            return None
+
+    def run(self, in_func, out_func):
+        break_encountered = False
+        while self.exec and not break_encountered:
+            op = self.tape[self.pc] % 100
+            if op not in self.instr_len:
+                print("UNKNOWN OPCODE %r" % op)
+                break
+            p_modes = ("%d" % (self.tape[self.pc])).zfill(self.instr_len[op]+2)[:self.instr_len[op]][::-1]
+            p = [self.get_addr(self.pc+i+1, int(p_modes[i])) for i in range(len(p_modes))]
+            # print(self.pc, op, self.memo[op], p_modes, [(pe, self.tape[pe]) for pe in p], self.rel_base)
+            if self.pc in self.breakpoints:
+                break_encountered = True
+            if op == 99:
+                break
+            elif op == 1:
+                self.tape[p[2]] = self.tape[p[0]] + self.tape[p[1]]
+                self.pc += 4
+            elif op == 2:
+                self.tape[p[2]] = self.tape[p[0]] * self.tape[p[1]]
+                self.pc += 4
+            elif op == 3:
+                self.tape[p[0]] = in_func()
+                self.pc += 2
+            elif op == 4:
+                out_func(self.tape[p[0]])
+                self.pc += 2
+            elif op == 5:
+                if self.tape[p[0]] != 0:
+                    self.pc = self.tape[p[1]]
+                else:
+                    self.pc += 3
+            elif op == 6:
+                if self.tape[p[0]] == 0:
+                    self.pc = self.tape[p[1]]
+                else:
+                    self.pc += 3
+            elif op == 7:
+                self.tape[p[2]] = 1 if self.tape[p[0]] < self.tape[p[1]] else 0
+                self.pc += 4
+            elif op == 8:
+                self.tape[p[2]] = 1 if self.tape[p[0]] == self.tape[p[1]] else 0
+                self.pc += 4
+            elif op == 9:
+                self.rel_base += self.tape[p[0]]
+                self.pc += 2
+            else:
+                print("AAAAAAA", self.pc, op)
+                break
+
+def nic_read(id):
+    global packet_queue, initialized, idle, NAT, last_NAT, end
+    if not initialized[id]:
+        initialized[id] = True
+        return id
+    else:
+        if len(packet_queue[id]) == 0:
+            idle[id] += 1
+            all_idle = True
+            for i in range(VM_NUM):
+                if idle[i] <= 1:
+                    all_idle = False
+                    break
+            if all_idle:
+                if NAT == last_NAT:
+                    end = True
+                else:
+                    last_NAT = NAT
+                    packet_queue[0].append(NAT)
+                    idle[0] = 0
+            vms[id].exec = False
+            return -1
+        idle[id] = 0
+        pack = packet_queue[id][0]
+        if pack[0] is not None:
+            ret = pack[0]
+            packet_queue[id][0] = (None, pack[1])
+            return ret
+        packet_queue[id].pop(0)
+        ret = pack[1]
+        vms[id].exec = False
+        return ret
+
+def nic_out(id, o):
+    global vm_buffer, packet_queue, NAT
+    vm_buffer[id].append(o)
+    if len(vm_buffer[id]) == 3:
+        if vm_buffer[id][0] == 255:
+            NAT = (vm_buffer[id][1], vm_buffer[id][2])
+        else:
+            packet_queue[vm_buffer[id][0]].append((vm_buffer[id][1], vm_buffer[id][2]))
+        vm_buffer[id] = []
+
+vms = [None for i in range(VM_NUM)]
+initialized = [False for i in range(VM_NUM)]
+packet_queue = [[] for i in range(VM_NUM)]
+vm_buffer = [[] for i in range(VM_NUM)]
+idle = [0 for i in range(VM_NUM)]
+NAT = (None, None)
+last_NAT = (None, None)
+end = False
+
+for i in range(VM_NUM):
+    vm = IntcodeVM(tape)
+    vms[i] = vm
+
+vm_ctr = 0
+while not end:
+    id = vm_ctr % VM_NUM
+    vms[id].exec = True
+    vms[id].run(lambda : nic_read(id), lambda o: nic_out(id, o))
+    vm_ctr += 1
+
+print(NAT[1])
